bebb25a71ecc7d3fd08dcaf2a733555b369261f8
Ferdinando Primerano <ferdinando.primerano@unep-wcmc.org>
2018-07-06 16:59:47 +0100

    First draft for pagination endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   get '/', to: 'home#index'
   post '/download', to: 'home#download'
 
+  get '/list', to: 'home#list'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
