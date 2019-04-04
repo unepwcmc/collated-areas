06c821391a200763a32e7791f7556459191397d2
Andrew Potter <hello@drewpotter.com>
2019-04-04 17:05:29 +0100

    Remove commented relationships from models

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,15 +1,11 @@
 class Country < ActiveRecord::Base
   has_and_belongs_to_many :protected_areas
 
-  # has_one :country_statistic
-  # has_one :pame_statistic
-
   belongs_to :region
   belongs_to :region_for_index, -> { select('regions.id, regions.name') }, :class_name => 'Region', :foreign_key => 'region_id'
 
   has_many :sub_locations
   has_many :designations, -> { uniq }, through: :protected_areas
-  # has_many :iucn_categories, through: :protected_areas
 
   belongs_to :parent, class_name: "Country", foreign_key: :country_id
   has_many :children, class_name: "Country"
