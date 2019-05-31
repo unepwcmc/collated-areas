0e5cd4a0509c7537c5b9a4a0910ba2df808db312
Andrew Potter <hello@drewpotter.com>
2019-05-31 15:44:21 +0100

    Improvement to test can filter countries for filters spec

--- spec/features/filters_spec.rb
+++ spec/features/filters_spec.rb
@@ -4,16 +4,14 @@ RSpec.feature "Filters", type: :feature do
   feature "can filter countries" do
     scenario "by country" do
       visit root_path
-      #click_on(class: "filter__button button filter__button--active")
-      #check('usa')
-      #save_screenshot
       find('.filter', text: 'Country').click
       save_screenshot
-      #find('.filter__checkbox', text: 'USA').check
-      #check('usa')
-      #click('Apply')
-      #expect(page).to have_content('USA')
-      #expect(page).to_not have_content('JPN')
+      find('.filter__option', text: 'USA', visible: false).check
+      save_screenshot
+      click_on('Apply')
+      save_screenshot
+      expect(page).to have_content('USA')
+      expect(page).to_not have_content('JPN')
     end
 
     scenario "by year"
