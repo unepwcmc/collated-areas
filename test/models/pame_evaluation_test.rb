c4368507cb15f249c6ef43437354b498a6316db3
Ben Tregenna <BenT@WCMC-LT-02195s-MacBook-Pro.local>
2019-07-10 10:24:40 +0100

    Added one simple test with FactoryGirl using pp docker test db

---
+++ test/models/pame_evaluation_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+
+class PameEvaluationTest < ActiveSupport::TestCase
+  DEFAULT_PARAMS =
+  {
+    requested_page: 1,
+    filters: []
+  }.to_json
+
+  test "basic test" do
+    pa = FactoryGirl.create(:protected_area, name: "Evaluated Area")
+    ps = FactoryGirl.create(:pame_source)
+    pe = FactoryGirl.create(:pame_evaluation, protected_area: pa, pame_source: ps)
+    result = PameEvaluation.paginate_evaluations(DEFAULT_PARAMS)
+    assert_equal 1, result[:total_entries]
+  end
+
+end
