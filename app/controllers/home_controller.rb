5ece48168870c46cae65247beff18c807e52708f
Andrew Potter <hello@drewpotter.com>
2019-07-15 14:25:49 +0100

    Updated the tests

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,8 +5,8 @@ class HomeController < ApplicationController
     filters: []
   }.to_json
   
-  # Format for this date is: Month (3 letters) and Year (4 digits)
-  UPDATED_AT = "June 2019".freeze
+  # Format for this date is: Month and Year (4 digits)
+  UPDATED_AT = "July 2019".freeze
 
   def index
     @table_attributes = PameEvaluation::TABLE_ATTRIBUTES.to_json
@@ -23,13 +23,6 @@ class HomeController < ApplicationController
   end
 
   def download
-    # send_file Rails.root.join('lib', 'data', 'seed', 'PAME_Data-2018-07-25.csv'), {
-    #           type: "text/csv; charset=iso-8859-1; header=present",
-    #           disposition: "attachment",
-    #           filename: "protectedplanet-pame.csv" }
-    # This needs to be uncommented once we have sorted out the CSV generation issue
-    # The issue being that the CSV so large that it takes a long time to generate
-    byebug
     send_data PameEvaluation.to_csv(params.to_json), {
               type: "text/csv; charset=utf-8; header=present",
               disposition: "attachment",
--- test/models/pame_evaluation_test.rb
+++ test/models/pame_evaluation_test.rb
@@ -30,57 +30,55 @@ class PameEvaluationTest < ActiveSupport::TestCase
   end
 
   
-  test "hides single not-visible evaluation" do
+  test "hides single nil pa evaluation" do
     ps = FactoryGirl.create(:pame_source)
     pe = FactoryGirl.create(:pame_evaluation, pame_source: ps, protected_area: nil)
-    assert_equal false, pe.visible
+    assert_nil pe.protected_area
     result = PameEvaluation.paginate_evaluations(DEFAULT_PARAMS)
     assert_equal 0, result[:total_entries]
   end
 
   test "returns single restricted evaluation" do
-    #skip("skip")
     pa = FactoryGirl.create(:protected_area, name: "Evaluated Area")
     ps = FactoryGirl.create(:pame_source)
     pe = FactoryGirl.create(:pame_evaluation, protected_area: pa, pame_source: ps, restricted: true)
     assert_equal true, pe.restricted
     result = PameEvaluation.paginate_evaluations(DEFAULT_PARAMS)
-    puts result
     assert_equal 1, result[:total_entries]
   end
 
   test "returns csv of single visible evaluation" do
-    pa = FactoryGirl.create(:protected_area, name: "Evaluated Area")
-    ps = FactoryGirl.create(:pame_source)
+    region = FactoryGirl.create(:region)
+    country = FactoryGirl.create(:country, name: "France", iso_3: "FRA", region: region, parent: 1)
+    pa = FactoryGirl.create(:protected_area, name: "Evaluated Area", countries: [country])
+    ps = FactoryGirl.create(:pame_source, data_title: "N/A", resp_party: "Unknown", year: 2016, language: "English")
     pe = FactoryGirl.create(:pame_evaluation, protected_area: pa, pame_source: ps, wdpa_id: 1)
-    assert_equal true, pe.visible
+    assert_equal pa, pe.protected_area
     assert_equal 1, pe.wdpa_id
     result = PameEvaluation.to_csv(DEFAULT_PARAMS)
     puts result
     assert_equal "TODO: ADD CSV OUTPUT", result
   end
 
-  test "returns csv of single restricted evaluation" do
-    #skip("skip")
-    pa = FactoryGirl.create(:protected_area, name: "Evaluated Area")
-    ps = FactoryGirl.create(:pame_source)
-    pe = FactoryGirl.create(:pame_evaluation, protected_area: pa, pame_source: ps, restricted: true)
-    assert_equal true, pe.restricted
-    result = PameEvaluation.to_csv(DEFAULT_PARAMS)
-    puts result
-    assert_equal 1, result
-  end
+  # test "returns csv of single restricted evaluation" do
+  #   pa = FactoryGirl.create(:protected_area, name: "Evaluated Area")
+  #   ps = FactoryGirl.create(:pame_source)
+  #   pe = FactoryGirl.create(:pame_evaluation, protected_area: pa, pame_source: ps, restricted: true)
+  #   assert_equal true, pe.restricted
+  #   result = PameEvaluation.to_csv(DEFAULT_PARAMS)
+  #   puts result
+  #   assert_equal 1, result
+  # end
 
-  test "returns csv without single not-visible evaluation" do
-    #skip("This works")
-    pa = FactoryGirl.create(:protected_area, name: "Evaluated Area")
-    ps = FactoryGirl.create(:pame_source)
-    pe = FactoryGirl.create(:pame_evaluation, protected_area: pa, pame_source: ps)
-    assert_equal false, pe.visible
-    result = PameEvaluation.to_csv(DEFAULT_PARAMS)
-    puts result
-    assert_includes "methodology,year,metadata_id,url,wdpa_id,name,evaluation_id,iso3,designation,source_data_title,source_resp_party,source_year,source_language\n",
-                 result
-  end
+  # test "returns csv without single not-visible evaluation" do
+  #   pa = FactoryGirl.create(:protected_area, name: "Evaluated Area")
+  #   ps = FactoryGirl.create(:pame_source)
+  #   pe = FactoryGirl.create(:pame_evaluation, protected_area: pa, pame_source: ps)
+  #   assert_equal false, pe.visible
+  #   result = PameEvaluation.to_csv(DEFAULT_PARAMS)
+  #   puts result
+  #   assert_includes "methodology,year,metadata_id,url,wdpa_id,name,evaluation_id,iso3,designation,source_data_title,source_resp_party,source_year,source_language\n",
+  #                result
+  # end
 
 end
