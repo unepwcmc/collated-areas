ef3af68f11417204d2aca8fa0dfe6ba3e82b4fff
Andrew Potter <hello@drewpotter.com>
2019-07-16 15:28:55 +0100

    Fix up the pame evaluation tests to be more concise

--- test/models/pame_evaluation_test.rb
+++ test/models/pame_evaluation_test.rb
@@ -33,7 +33,6 @@ class PameEvaluationTest < ActiveSupport::TestCase
   test "hides single nil pa evaluation" do
     ps = FactoryGirl.create(:pame_source)
     pe = FactoryGirl.create(:pame_evaluation, pame_source: ps, protected_area: nil)
-    assert_nil pe.protected_area
     result = PameEvaluation.paginate_evaluations(DEFAULT_PARAMS)
     assert_equal 0, result[:total_entries]
   end
@@ -42,7 +41,6 @@ class PameEvaluationTest < ActiveSupport::TestCase
     pa = FactoryGirl.create(:protected_area, name: "Evaluated Area")
     ps = FactoryGirl.create(:pame_source)
     pe = FactoryGirl.create(:pame_evaluation, protected_area: pa, pame_source: ps, restricted: true)
-    assert_equal true, pe.restricted
     result = PameEvaluation.paginate_evaluations(DEFAULT_PARAMS)
     assert_equal 1, result[:total_entries]
   end
@@ -53,8 +51,6 @@ class PameEvaluationTest < ActiveSupport::TestCase
     pa = FactoryGirl.create(:protected_area, name: "Evaluated Area", countries: [country])
     ps = FactoryGirl.create(:pame_source, data_title: "N/A", resp_party: "Unknown", year: 2016, language: "English")
     pe = FactoryGirl.create(:pame_evaluation, id: 1, protected_area: pa, pame_source: ps, wdpa_id: 1)
-    assert_equal pa, pe.protected_area
-    assert_equal 1, pe.wdpa_id
     result = PameEvaluation.to_csv(DEFAULT_PARAMS)
     actual_csv = File.open('lib/data/seed/test_pame_returns_csv_of_single_visible_evaluation.csv').read
     assert_equal(result, actual_csv)
@@ -66,17 +62,14 @@ class PameEvaluationTest < ActiveSupport::TestCase
     pa = FactoryGirl.create(:protected_area, name: "Evaluated Area", countries: [country])
     ps = FactoryGirl.create(:pame_source, data_title: "N/A", resp_party: "Unknown", year: 2016, language: "English")
     pe = FactoryGirl.create(:pame_evaluation, id: 1, protected_area: pa, pame_source: ps, restricted: true, wdpa_id: 1)
-    assert_equal true, pe.restricted
     result = PameEvaluation.to_csv(DEFAULT_PARAMS)
     actual_csv = File.open('lib/data/seed/test_pame_returns_csv_of_single_restricted_evaluation.csv').read
     assert_equal(result, actual_csv)
   end
 
   test "returns csv without single not-visible evaluation" do
-    pa = nil
     ps = FactoryGirl.create(:pame_source)
-    pe = FactoryGirl.create(:pame_evaluation, protected_area: pa, pame_source: ps)
-    assert_equal false, pe.protected_area_id.present?
+    pe = FactoryGirl.create(:pame_evaluation, protected_area: nil, pame_source: ps)
     result = PameEvaluation.to_csv(DEFAULT_PARAMS)
     actual_csv = File.open('lib/data/seed/test_pame_returns_csv_without_single_not-visible_evaluation.csv').read
     assert_equal(result, actual_csv)
