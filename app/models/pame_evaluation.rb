56864fec5ce2396935589918493f85a1e531c547
Andrew Potter <hello@drewpotter.com>
2019-07-17 16:07:52 +0100

    Add tests and make sure they pass

--- app/models/pame_evaluation.rb
+++ app/models/pame_evaluation.rb
@@ -215,7 +215,7 @@ class PameEvaluation < ApplicationRecord
       evaluation_columns = PameEvaluation.new.attributes.keys
       evaluation_columns << "evaluation_id"
 
-      excluded_attributes = ["restricted", "protected_area_id", "pame_source_id", "created_at", "updated_at", "id", "site_id", "source_id"]
+      excluded_attributes = ["assessment_is_public", "restricted", "protected_area_id", "pame_source_id", "created_at", "updated_at", "id", "site_id", "source_id"]
 
       evaluation_columns.delete_if { |k, v| excluded_attributes.include? k }
 
--- test/models/pame_evaluation_test.rb
+++ test/models/pame_evaluation_test.rb
@@ -85,10 +85,9 @@ class PameEvaluationTest < ActiveSupport::TestCase
   test "returns a link when assessment_is_public is true and url has a url" do
     pa = FactoryGirl.create(:protected_area, name: "Evaluated Area")
     ps = FactoryGirl.create(:pame_source)
-    pe = FactoryGirl.create(:pame_evaluation, protected_area: pa, pame_source: ps, assessment_is_public: true)
+    pe = FactoryGirl.create(:pame_evaluation, protected_area: pa, pame_source: ps, url: "https://wcmc.io", assessment_is_public: true)
     result = PameEvaluation.paginate_evaluations(DEFAULT_PARAMS)
-    puts result
-    assert_equal 1, result[:total_entries]
+    assert_equal "https://wcmc.io",  result[:items].first[:url]
   end
 
   test "returns not currently public when assessment_is_public is true and url is blank" do
@@ -104,7 +103,15 @@ class PameEvaluationTest < ActiveSupport::TestCase
   end
 
   test "csv returns a link when assessment_is_public is true and url has a url" do
-    skip("skip")
+    region = FactoryGirl.create(:region)
+    country = FactoryGirl.create(:country, name: "France", iso_3: "FRA", region: region)
+    pa = FactoryGirl.create(:protected_area, name: "Evaluated Area", countries: [country])
+    ps = FactoryGirl.create(:pame_source, data_title: "N/A", resp_party: "Unknown", year: 2016, language: "English")
+    pe = FactoryGirl.create(:pame_evaluation, protected_area: pa, pame_source: ps, wdpa_id: 1, id: 1, 
+                                              url: "https://wcmc.io", assessment_is_public: true)
+    actual_csv = File.open('lib/data/seed/test_pame_csv_returns_a_link_when_assessment_is_public_is_true_and_url_has_a_url.csv').read
+    result = PameEvaluation.to_csv(DEFAULT_PARAMS)
+    assert_equal(result, actual_csv)
   end
 
   test "csv returns not currently public when assessment_is_public is true and url is blank" do
